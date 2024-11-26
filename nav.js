adf523041437a093321a143989831d6b0e39d1e6
Agustin Hofmann <166507337+AgustinHofmann@users.noreply.github.com>
2024-11-26 17:17:25 -0300

    Chequea que haya usuario logueado, si no redirije a LogIn
    Para que esto funcione hay que chequear que la variable projectFolder tenga un contenido correcto!

--- nav.js
+++ nav.js
@@ -32,8 +32,28 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Al cargar la página, recuperar la imagen y mostrarla
+    
     const user = obtenerUsuario();
+
+    // Chequear que hay un usuario logueado.
+    if (!user)
+    {                
+        const currentUrl = window.location.href;
+        
+        const projectFolder = "proyecto-3-coudy";
+        const projectIndex = currentUrl.indexOf(projectFolder);
+
+        if (projectIndex !== -1) {
+            const basePath = currentUrl.substring(0, projectIndex + projectFolder.length);
+            const loginPath = basePath + "/Log_In/index.html";
+            
+            window.location.href = loginPath;
+        } else {
+            console.log(`La carpeta "${projectFolder}" no fue encontrada en el URL.`);
+        }
+    }
+
+    // Al cargar la página, recuperar la imagen y mostrarla
     const savedImage = user.perfilImagen;
 
     if (savedImage) {
