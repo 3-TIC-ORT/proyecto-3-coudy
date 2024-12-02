41f20f525a6c3a216b9e4b17cca5e1b0e4ceeb3a
Agustin Hofmann <agustinhofmann@gmail.com>
2024-12-02 10:46:56 -0300

    Arregle seleccion de niveles manual

---
+++ Seleccion_de_niveles-Manual/index.js
@@ -0,0 +1,9 @@
+function guardarInfo(num){
+    if(num === 1){
+        window.location.href='../HTML\'S_FOLDER/HTML-1/index.html'
+    } else if(num === 2){
+        window.location.href='../CSS\'S_FOLDER/CSS-1/index.html'
+    } else if(num = 3){
+        window.location.href='../JS\'S_FOLDER/JS-1/index.html'
+    }            
+}
